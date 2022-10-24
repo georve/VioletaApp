fb6f9ef3af0a7bfc33edf4605672371416d0fe4c
Georman <georman.calderon@cleteci.com>
2022-10-24 18:22:09 -0400

    insert

--- src/screens/SOSButton/SOSButton.js
+++ src/screens/SOSButton/SOSButton.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {ext, View} from 'react-native';
+import {Text, View} from 'react-native';
 export default class SOSButton extends Component {
   constructor(props) {
     super(props);
